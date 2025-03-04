4065101210baa238e290ee308caa7adce1f306b8
Absalone <axelordend@gmail.com>
2025-03-04 12:00:10 -0500

    modified_week_1A/extensions/extensions.py

--- week_1A/extensions/extensions.py
+++ week_1A/extensions/extensions.py
@@ -9,10 +9,25 @@ def cleaner(file):
     file="".join(file)
     return file
 
+#.gif.jpg.jpeg.png.pdf.txt.zip
 def file_disc(file):
     if ".py" in file:
         return "program/python"
-    elif".jpg" in file:
-        return "image/jpg"
-
+    elif ".gif" in file:
+        return "image/gif"
+    elif ".jpg" in file:
+        return "image/jpeg"
+    elif ".jpeg" in file:
+        return "image/jpeg"
+    elif ".png" in file:
+        return "image/png"
+    elif ".pdf" in file:
+        return "application/pdf"
+    elif ".txt" in file:
+        return "text/plain"
+    elif ".zip" in file:
+        return "application/zip"
+    else:
+        return "application/octet-stream"
 main()
+#check50 cs50/problems/2022/python/extensions
