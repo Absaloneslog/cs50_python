798807db02326c06873aa4f0344a88802f145ee3
Absalone <axelordend@gmail.com>
2025-03-08 23:14:35 -0500

    modified meal.py code_1.py

--- code_1.py
+++ code_1.py
@@ -3,12 +3,9 @@
 #user_input.count(" ")
 #print(f"{a}")
 
-hello=input("")
-#if not ("am" in hello or "pm" in hello
-# ):
-#    print(f"hello")
-#else:
-#answer=hello.strip(" .am")
-#print(f"{answer}2")
-x=float(hello)
-print(f"{x}")
+hello=input("").strip()
+hello=hello.replace(" ", "")
+answer=hello.strip(" p.m.")
+print(f"{answer}2")
+#x=float(hello)
+
--- week_1A/meal/meal.py
+++ week_1A/meal/meal.py
@@ -3,19 +3,23 @@ print("Please use whatever of the following formats  example:")
 print("##:## HH:MM or ##:## a.m.")
 def main():
     # Ask for input.
-    time=input(" What time is it? ").strip(" ")
-    if not ":" in time:
-        print("Please use the previous examples")
-    elif time=="":
+    time=input(" What time is it? ").strip()
+    time=time.replace(" ","")
+    if time=="":
         print("")
+    elif not ":" in time:
+        print("Please use the format of the previous examples")
     else:
     #Checking the format
         approval=check_in(time)
         match approval:
             case "number_error":
                 print("Please only use numbers")
+            case "format_error":
+                print("Please use the format of the previous examples")
             case "Yes":
                 hour=convert(time)
+                #print(f"{hour}")
                 machine_answer=match_machine(hour)
                 print(f"{machine_answer}")
 
@@ -37,7 +41,7 @@ def check_in(time):
     else:
         x,y=time.split(":")
         if not (x.isdigit() and y.isdigit()):
-            return "Please use the previous examples"
+            return "format_error"
         else:
             return "Yes"
 
@@ -47,14 +51,24 @@ def convert(time):
         x,y=time_am.split(":")
         x=float(x)
         y=float(y)/60
-        z=x+y
-        return z
+        if x==12:
+                x=x-12
+                z=x+y
+                return z
+        else:
+            z=x+y
+            return z
     elif "p.m." in time:
         time_pm=time.strip("p.m.")
         x,y=time_pm.split(":")
         x=float(x)+12
         y=float(y)/60
-        z=x+y
+        if x==24:
+                x=float(x)-12
+                z=x+y
+                return z
+        else:
+            z=x+y
         return z
     else:
         x,y=time.split(":")
